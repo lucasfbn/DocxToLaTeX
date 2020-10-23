91ba6a481d91ff5c07f9c5de5c240176f89cb17f
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2020-10-23 15:50:44 +0200

    Made the conversion of docx newlines to tex newlines optional

--- docx_to_latex/docx_latex_converter.py
+++ docx_to_latex/docx_latex_converter.py
@@ -6,17 +6,19 @@ from docx_to_latex.latex import LaTex
 
 class DocxToLaTeX:
 
-    def __init__(self, docx_path, tex_template_path, pdf_out_path=None, save_rendered_template=False):
+    def __init__(self, docx_path, tex_template_path, pdf_out_path=None, save_rendered_template=False,
+                 convert_newlines=False):
         self.docx_in_path = docx_path
         self.tex_template_path = tex_template_path
         self.pdf_out_path = pdf_out_path
 
         self._save_rendered_template = save_rendered_template
 
+        self._convert_newlines = convert_newlines
         self.parse_dict = self._load_docx()
 
     def _load_docx(self):
-        return Docx(self.docx_in_path).parse()
+        return Docx(self.docx_in_path).parse(self._convert_newlines)
 
     def add_custom_key_val(self, key, val):
         if key in self.parse_dict:
